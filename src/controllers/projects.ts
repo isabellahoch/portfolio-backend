45cc0ec44bacafb591778fc752302bd78964bfe2
isabellahoch <40366749+isabellahoch@users.noreply.github.com>
2023-09-24 16:51:43 -0400

    feat: begin process of connecting to AWS bucket

--- src/controllers/projects.ts
+++ src/controllers/projects.ts
@@ -11,34 +11,32 @@ export const fetchProjects = (req: Request, res: Response) => {
     {
       "id": 1,
       "title": "Diatribe Internal Data Dashboard",
-      "description": "Leveraged Mailchimp API to develop comprehensive Python/Flask internal data dashboard for diabetes-focused healthcare consulting agency with xx email subscribers",
-      "year": 2018
+      "description": "Leveraged Mailchimp API to develop comprehensive Python/Flask internal data dashboard for diabetes-focused healthcare consulting agency with 325,000+ email subscribers",
+      "year": 2018,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/dt_mailchimp_metrics.png"
     },
     {
       "id": 2,
-      "title": "incub8",
-      "description": "Created web app and mobile app for novel “Tinder for startups and investors” concept centered around uplifting female founders who traditionally receive only x% of funding through traditional VC channels. Placed 1st in Congressional App Challenge (District CA-15).",
-      "year": 2018
-    },
-    {
-      "id": 3,
-      "title": "Toxic Instagram Agent",
-      "description": "Created web scraping algorithm to determine the unreciprocal accounts a user is following but who do not follow them back (unidirectional edges on the social network graph).",
-      "year": 2018
+      "title": "1ncub8",
+      "description": "Created web app and mobile app for novel “Tinder for startups and investors” concept centered around empowering female founders who receive only 2.7% of funding through traditional VC channels. Placed 1st in Congressional App Challenge (District CA-15).",
+      "year": 2018,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/1NCUB8.png"
     },
     {
       "id": 4,
       "title": "Gluten VR Simulator",
-      "description": "Developed 3D gluten simulator web app allowing users to visualize and interact with gluten molecules through an easy-to-use interface.",
+      "description": "Developed VR gluten simulator web app allowing users to visualize and interact with 3D gluten molecules through an easy-to-use interface.",
       "year": 2019,
-      "githubUrl": "https://github.com/isabellahoch/chem_project"
+      "githubUrl": "https://github.com/isabellahoch/chem_project",
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/LetsGetThisBread.png"
     },
     {
       "id": 5,
       "title": "buddi",
       "description": "Created accessibility-forward iOS app for senior population using Swift/Xcode/Cocoa, aggregating COVID-19 resources through a customized engaging intent-based conversational AI chatbot. Implemented Firebase authentication flow.",
       "year": 2020,
-      "githubUrl": "https://github.com/isabellahoch/buddi"
+      "githubUrl": "https://github.com/isabellahoch/buddi",
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/Buddi.png"
     },
     {
       "id": 6,
@@ -58,30 +56,29 @@ export const fetchProjects = (req: Request, res: Response) => {
       "id": 8,
       "title": "sendavirtualhug.com",
       "description": "During the COVID-19 pandemic, developed Python REST API allowing users to request virtual hugs to be sent with custom messaging and timeline, with the end goal of connecting users and bringing cheer during a challenging period. Created accompanying Flask frontend.",
-      "year": 2021
+      "year": 2021,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/SendAVirtualHug.png"
     },
     {
       "id": 9,
       "title": "Alexa Skill",
       "description": "Developed 5-star Alexa Skill offering custom birth control reminders and accessible reproductive health information. Implemented 20+ intent handlers with Python ASK SDK. Featured on Alexa Skills Kit blog.",
-      "year": 2021
+      "year": 2021,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/Nurx+Alexa+Skill.png"
     },
     {
       "id": 10,
       "title": "fightfastfashion.com",
       "description": "Developed macro-search engine scraping results from green websites to find sustainable alternatives to fast fashion items.",
-      "year": 2021
-    },
-    {
-      "id": 11,
-      "title": "Viterbi Decoder Algorithm",
-      "description": "Developed Viterbi decoder algorithm in 2021."
+      "year": 2021,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/FightFastFashion.png"
     },
     {
       "id": 12,
       "title": "PineTime",
-      "description": "Created Python/Flask web app aggregating student events on campus and sending out an automated daily digest newsletter, leveraging Google Calendar API.",
-      "year": 2021
+      "description": "Created Python/Flask web app & accompanying Alexa Skill aggregating student events on campus and sending out an automated daily digest newsletter, leveraging Google Calendar API.",
+      "year": 2021,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/PineTime.png"
     },
     {
       "id": 13,
@@ -93,27 +90,31 @@ export const fetchProjects = (req: Request, res: Response) => {
       "id": 14,
       "title": "DALI Museum",
       "description": "Developed NodeJS REST API for novel augmented reality museum exhibit showcasing 30+ paintings. Integrated backend with Unity frontend using C#.",
-      "year": 2022
+      "year": 2022,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/HowDoILook.png"
     },
     {
       "id": 15,
       "title": "DartPass",
       "description": "Placed first in 10-week TuckLAB (Liberal Arts & Business) entrepreneurship-focused program through Dartmouth's Tuck School of Business for DartPass, the digital key to Dartmouth.",
       "year": 2022,
-      "githubUrl": "https://github.com/isabellahoch/dartpass-website"
+      "githubUrl": "https://github.com/isabellahoch/dartpass-website",
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/DartPass.png"
     },
     {
       "id": 16,
       "title": "Coursera MOOC",
       "description": "Developed comprehensive curriculum and testing for Coursera’s first collaboration with Dartmouth faculty, publishing material from COSC52: Full-Stack Web Development and writing tests in Jest.",
       "year": 2022,
-      "githubUrl": "https://home.dartmouth.edu/news/2021/11/dartmouth-partner-coursera-online-learning"
+      "githubUrl": "https://home.dartmouth.edu/news/2021/11/dartmouth-partner-coursera-online-learning",
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/Coursera+MOOC.png"
     },
     {
       "id": 17,
       "title": "Munch",
       "description": "Created novel food-centric React Native social media app allowing users to share recipes and discover new foods. Developed RESTful NodeJS backend.",
-      "year": 2022
+      "year": 2022,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/CS52+Munch.png"
     },
     {
       "id": 18,
@@ -131,7 +132,8 @@ export const fetchProjects = (req: Request, res: Response) => {
       "id": 20,
       "title": "SpeakUp",
       "description": "Partnered with Dartmouth Linguistics professors to develop NodeJS backend for React Native app innovating the arduous task of collecting data for linguistics research, cultivating the largest-ever sample of audio recordings from New England area dialects to be used for novel computational linguistics research.",
-      "year": 2022
+      "year": 2022,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/Linguistics+Games.png"
     },
     {
       "id": 21,
@@ -143,18 +145,21 @@ export const fetchProjects = (req: Request, res: Response) => {
       "id": 22,
       "title": "Orbit",
       "description": "Led team of software engineers and designers to create high-fidelity prototype for early-stage social networking startup inspiring users to build friendships through mutual connections. Developed BFS-adjacent graph traversal algorithm matching users with second and third degree connections.",
-      "year": 2023
+      "year": 2023,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/Orbit.png"
     },
     {
       "id": 23,
       "title": "Dartmouth ITC Major Declaration Process",
       "description": "Worked directly with Dartmouth ITC to streamline major declaration process and make it more intuitive for students. Led team of developers to develop customized React frontend and JavaScript data parser to plug into school database.",
-      "year": 2023
+      "year": 2023,
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/ITC+Major+Declaration.png"
     },
     {
       "id": 24,
       "title": "Undisclosed Stealth Startup",
       "description": "Served as founding engineer of seed-stage stealth startup operating in the social networking space. Developed React Native app and accompanying web app using Firebase Auth, Firestore, Cloud Functions. Integrated CometChat webhook messaging API.",
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/Stealth.png",
       "year": 2023
     },
     {
@@ -162,7 +167,8 @@ export const fetchProjects = (req: Request, res: Response) => {
       "title": "IoT Mushroom Lamp",
       "description": "Designed and engineered 3D-printed full-stack Internet of Things lamp using ESP32-C3 microcontroller to wirelessly control LEDs, leveraging Arduino IDE, React.js frontend, Node.js backend, and websockets.",
       "year": 2023,
-      "githubUrl": "https://github.com/isabellahoch/cs29-dig-fab-IoT-mushroom-lamp"
+      "githubUrl": "https://github.com/isabellahoch/cs29-dig-fab-IoT-mushroom-lamp",
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/MushroomLamp.png"
     },
     {
       "id": 26,
@@ -170,7 +176,8 @@ export const fetchProjects = (req: Request, res: Response) => {
       "description": "Developed frontend and backend for text-to-image generative AI web app leveraging stable diffusion to render impressionist-style paintings according to user input. Finalist at 2023 AI Genesis Hackathon.",
       "year": 2023,
       "liveUrl": "https://ai-impressionism.onrender.com/",
-      "githubUrl": "https://github.com/isabellahoch/generative-ai-genesis-hackathon"
+      "githubUrl": "https://github.com/isabellahoch/generative-ai-genesis-hackathon",
+      "imageUrl": "https://isabellahoch-portfolio-bucket.s3.us-east-2.amazonaws.com/projects/ImpressionAI.png"
     }
   ].reverse()
   );
